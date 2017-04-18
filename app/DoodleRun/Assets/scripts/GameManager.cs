df3ee9f813b7f36e84c050baff43c2062696a668
johnnycchi <johnnycchi@gmail.com>
2017-04-18 10:48:18 -0700

    04/18 - added catcher and started working on restart

---
+++ app/DoodleRun/Assets/scripts/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+
+	public PlayerController thePlayer;
+	public Vector3 playerStartPoint;
+
+	// Use this for initialization
+	void Start () {
+		playerStartPoint = thePlayer.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void RestartLevel() {
+		StartCoroutine ("RestartLevelCo");
+	}
+
+	public IEnumerator RestartLevelCo() {
+		thePlayer.gameObject.SetActive (false);
+		yield return new WaitForSeconds (0.5f);
+		thePlayer.transform.position = playerStartPoint;
+		thePlayer.gameObject.SetActive (true);
+	}
+}
